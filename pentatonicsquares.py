f32bdfc260465d1d26b66a105b44e23f8c10d248
Joseph Marchese <joeymarchese@gmail.com>
2017-01-27 19:43:27 -0500

    add all MIDI instruments, fix instrument mapping

--- pentatonicsquares.py
+++ pentatonicsquares.py
@@ -41,8 +41,29 @@ class pentatonicsquares:
         'Church Organ', 'Reed Organ', 'Accordion', 'Harmonica', 'Tango Accordion',
         'Acoustic Guitar (nylon)', 'Acoustic Guitar (steel)', 'Electric Guitar (jazz)',
         'Electric Guitar (clean)', 'Electric Guitar (muted)', 'Overdriven Guitar',
-        'Distortion Guitar', 'Guitar Harmonics']
+        'Distortion Guitar', 'Guitar Harmonics', 'Acoustic Bass',
+        'Electric Bass (finger)', 'Electric Bass (pick)', 'Fretless Bass',
+        'Slap Bass 1', 'Slap Bass 2', 'Synth Bass 1', 'Synth Bass 2', 'Violin',
+        'Viola', 'Cello', 'Contrabass', 'Tremolo Strings', 'Pizzicato Strings',
+        'Orchestral Harp', 'Timpani', 'String Ensemble 1', 'String Ensemble 2',
+        'Synth Strings 1', 'Synth Strings 2', 'Choir Aahs', 'Choir Oohs',
+        'Synth Voice', 'Orchestra Hit', 'Trumpet', 'Trombone', 'Tuba', 'Muted Trumpet',
+        'French Horn', 'Brass Section', 'Synth Brass 1', 'Synth Brass 2',
+        'Soprano Sax', 'Alto Sax', 'Tenor Sax', 'Baritone Sax', 'Oboe', 'English Horn',
+        'Bassoon', 'Clarinet', 'Piccolo', 'Flute', 'Recorder', 'Pan Flute',
+        'Blown Bottle', 'Shakuhachi', 'Whistle', 'Ocarina', 'Lead 1 (square)',
+        'Lead 2 (sawtooth)', 'Lead 3 (calliope)', 'Lead 4 (chiff)', 'Lead 5 (charang)',
+        'Lead 6 (voice)', 'Lead 7 (fifths)', 'Lead 8 (bass + lead)', 'Pad 1 (new age)',
+        'Pad 2 (warm)', 'Pad 3 (polysynth)', 'Pad 4 (choir)', 'Pad 5 (bowed)',
+        'Pad 6 (metallic)', 'Pad 7 (halo)', 'Pad 8 (sweep)', 'FX 1 (rain)',
+        'FX 2 (soundtrack)', 'FX 3 (crystal)', 'FX 4 (atmosphere)', 'FX 5 (brightness)',
+        'FX 6 (goblins)', 'FX 7 (echoes)', 'FX 8 (sci-fi)', 'Sitar', 'Banjo',
+        'Shamisen', 'Koto', 'Kalimba', 'Bag pipe', 'Fiddle', 'Shanai', 'Tinkle Bell',
+        'Agogo', 'Steel Drums', 'Woodblock', 'Taiko Drum', 'Melodic Tom', 'Synth Drum',
+        'Reverse Cymbal', 'Guitar Fret Noise', 'Breath Noise', 'Seashore', 'Bird Tweet',
+        'Telephone Ring', 'Helicopter', 'Applause', 'Gunshot']
 
+        print(len(self.instrument_list))
         self.instruments = Menu(self.menubar)
         self.menubar.add_cascade(menu=self.instruments, label='Instruments')
 
@@ -65,6 +86,66 @@ class pentatonicsquares:
         self.instruments.add_cascade(menu=self.guitar_menu, label='Guitar')
         for guitar in self.instrument_list[24:32]:
             self.guitar_menu.add_command(label=guitar, command=lambda i=guitar: self.select_instrument(i))
+
+        self.bass_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.bass_menu, label='Bass')
+        for bass in self.instrument_list[32:40]:
+            self.bass_menu.add_command(label=bass, command=lambda i=bass: self.select_instrument(i))
+
+        self.strings_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.strings_menu, label='Strings')
+        for string in self.instrument_list[40:48]:
+            self.strings_menu.add_command(label=string, command=lambda i=string: self.select_instrument(i))
+
+        self.ensemble_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.ensemble_menu, label='Ensemble')
+        for ensemble in self.instrument_list[48:56]:
+            self.ensemble_menu.add_command(label=ensemble, command=lambda i=ensemble: self.select_instrument(i))
+
+        self.brass_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.brass_menu, label='Brass')
+        for brass in self.instrument_list[56:64]:
+            self.brass_menu.add_command(label=brass, command=lambda i=brass: self.select_instrument(i))
+
+        self.reed_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.reed_menu, label='Reed')
+        for reed in self.instrument_list[64:72]:
+            self.reed_menu.add_command(label=reed, command=lambda i=reed: self.select_instrument(i))
+
+        self.pipe_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.pipe_menu, label='Pipe')
+        for pipe in self.instrument_list[72:80]:
+            self.pipe_menu.add_command(label=pipe, command=lambda i=pipe: self.select_instrument(i))
+
+        self.synth_lead_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.synth_lead_menu, label='Synth Lead')
+        for synth in self.instrument_list[80:88]:
+            self.synth_lead_menu.add_command(label=synth, command=lambda i=synth: self.select_instrument(i))
+
+        self.synth_pad_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.synth_pad_menu, label='Synth Pad')
+        for synth in self.instrument_list[88:96]:
+            self.synth_pad_menu.add_command(label=synth, command=lambda i=synth: self.select_instrument(i))
+
+        self.synth_effects_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.synth_effects_menu, label='Synth Effects')
+        for synth in self.instrument_list[96:104]:
+            self.synth_effects_menu.add_command(label=synth, command=lambda i=synth: self.select_instrument(i))
+
+        self.world_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.world_menu, label='World')
+        for world in self.instrument_list[104:112]:
+            self.world_menu.add_command(label=world, command=lambda i=world: self.select_instrument(i))
+
+        self.percussion_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.percussion_menu, label='Percussion')
+        for perc in self.instrument_list[112:120]:
+            self.percussion_menu.add_command(label=perc, command=lambda i=perc: self.select_instrument(i))
+
+        self.sound_effects_menu = Menu(self.menubar)
+        self.instruments.add_cascade(menu=self.sound_effects_menu, label='Sound Effects')
+        for sound in self.instrument_list[120:128]:
+            self.sound_effects_menu.add_command(label=sound, command=lambda i=sound: self.select_instrument(i))
 
         # Scales
         self.scales = Menu(self.menubar)
@@ -132,7 +213,8 @@ class pentatonicsquares:
         self.notes = [100, 103, 107, 110]
 
     def select_instrument(self, instrument):
-        self.player.set_instrument(self.instrument_list.index(instrument) + 1, 1)
+        print(instrument, print(self.instrument_list.index(instrument)))
+        self.player.set_instrument(self.instrument_list.index(instrument), 1)
 
     def reset(self):
         self.player.close()
